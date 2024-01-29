67496f8b4f318881d26f3e68dd77814e1c1abc5e
JoaoVitorLima242 <jlima0500@gmail.com>
2024-01-29 16:46:00 -0300

    feat: showing data in console

--- shared/base/tableComponent.mjs
+++ shared/base/tableComponent.mjs
@@ -4,4 +4,8 @@ export default class TableComponent {
   render(data) {
     throw new NotImplementedException(this.render.name);
   }
+
+  prepareData(data) {
+    throw new NotImplementedException(this.prepareData.name);
+  }
 }
